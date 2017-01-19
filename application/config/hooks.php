945ab95079f1d643c89a312555d615364f3f48fb
velioo <velioocs@mail.bg>
2017-01-19 23:59:54 +0200

    added config files

---
+++ application/config/hooks.php
@@ -0,0 +1,7 @@
+<?php
+$hook['post_controller'] = array(
+        'class'    => 'UserActivity',
+        'function' => 'update_user_activity',
+        'filename' => 'UserActivity.php',
+        'filepath' => 'hooks',
+);
